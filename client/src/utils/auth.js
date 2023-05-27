82bb4c50a613884b4b2d58e0644bdc22447e4242
lingeorge88 <lingeorge04@gmail.com>
2023-05-26 18:44:11 -0700

    updated API file and auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -9,12 +9,23 @@ class AuthService {
   }
 
   // check if user's logged in
+  // loggedIn() {
+  //   // Checks if there is a saved token and it's still valid
+  //   const token = this.getToken();
+  //   return !!token && !this.isTokenExpired(token); // handwaiving here
+  // }
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    if (!token) {
+      return false;
+    } else if (this.isTokenExpired(token)) {
+      localStorage.removeItem('id_token');
+      return false;
+    } else {
+      return true;
+    }
   }
-
   // check if token is expired
   isTokenExpired(token) {
     try {
@@ -45,5 +56,6 @@ class AuthService {
     window.location.assign('/');
   }
 }
+const AuthService = new AuthService();
 
 export default new AuthService();
